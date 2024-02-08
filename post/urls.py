2c310d94b190364bd2d2b17e77e4ec8ba63a4c9d
4Franco-star <emf0911b@gmail.com>
2024-02-08 20:03:28 +0900

    URLパターン追加

--- post/urls.py
+++ post/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 app_name = "post"
 urlpatterns = [
-    path("all/", views.AllView.as_view(), name="all"),
+    path("Income/all/", views.Post_Income_AllView.as_view(), name="Income_all"),
+    path("Expense/all/", views.Post_Expense_AllView.as_view(), name="Expense_all"),
+    path("create/", views.InExRegisterView.as_view(), name="create"),
+    path("Income/<int:pk>/delete/", views.Income_DeleteView.as_view(), name="Income_delete"),
+    path("Expense/<int:pk>/delete/", views.Expense_DeleteView.as_view(), name="Expense_delete"),
+    path("Income/<int:pk>/update/", views.Income_UpdateView.as_view(), name="Income_update"),
+    path("Expense/<int:pk>/update/", views.Expense_UpdateView.as_view(), name="Expense_update"),
+
 ]
